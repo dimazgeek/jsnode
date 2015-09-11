f3c165f9f6940f606ddfd5af2edbed4c4d36ffe9
tandhy <mail@tandhy.com>
2015-09-11 08:41:01 +0700

    add folder

---
+++ fizzbuzz/js/main.js
@@ -0,0 +1,30 @@
+
+
+// fizzbuzz
+
+window.onload = function(){ 
+    
+	var n = 30;
+	
+	for(var i =1; i<n; i++){
+		
+		if(i % 3 === 0 && i % 5 === 0)
+		{
+			console.log("fizzbuzz");
+		
+		}
+		 else if(i % 3 ===0)
+		{
+			console.log("fizz");
+		}
+		else if(i % 5 ===0){
+			console.log("buzz");
+		}
+		else {
+			console.log(i)
+		}
+		
+	}
+	
+                
+};
